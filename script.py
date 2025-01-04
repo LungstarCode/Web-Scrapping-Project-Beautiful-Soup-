92bb173f547f7c94aac3d6ae0f6ffe3af63a07e0
Lungile Mawila <lungilemawila5@gmail.com>
2025-01-04 14:33:30 +0200

    update: get data from HTML

--- script.py
+++ script.py
@@ -1,1 +1,7 @@
-# hello there!
+import requests
+
+# getting data from HTML document
+URL = 'https://www.geeksforgeeks.org/implementing-web-scraping-python-beautiful-soup/'
+r = requests.get(URL)
+
+print(r.content)
