0a1fba4d5d2510466931b51c1a0900930a3d619a
Lungile Mawila <lungilemawila5@gmail.com>
2025-01-04 14:45:49 +0200

    update: created a bs object and a parser

--- script.py
+++ script.py
@@ -1,7 +1,17 @@
-import requests
 
+# step 1: imports
+import requests
+from bs4 import BeautifulSoup
+
+# step 2: Extract HTML
 # getting data from HTML document
 URL = 'https://www.geeksforgeeks.org/implementing-web-scraping-python-beautiful-soup/'
 r = requests.get(URL)
 
-print(r.content)
+# Step 3: Parsing the HTML conent 
+
+soup = BeautifulSoup(r.content , 'html5lib')  # creates a bs object and specify the parser 
+print(soup.prettify())  # prints the html content
+
+
+
