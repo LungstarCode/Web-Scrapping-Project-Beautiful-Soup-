b8048248b9a984c0428565b6d55b9fa8fdee5076
Lungile Mawila <lungilemawila5@gmail.com>
2025-01-04 15:47:46 +0200

    scrapped the qoutes

--- script.py
+++ script.py
@@ -5,13 +5,32 @@ from bs4 import BeautifulSoup
 
 # step 2: Extract HTML
 # getting data from HTML document
-URL = 'https://www.geeksforgeeks.org/implementing-web-scraping-python-beautiful-soup/'
+URL = 'http://quotes.toscrape.com/'
 r = requests.get(URL)
 
 # Step 3: Parsing the HTML conent 
 
 soup = BeautifulSoup(r.content , 'html5lib')  # creates a bs object and specify the parser 
-print(soup.prettify())  # prints the html content
+#print(soup.prettify())  # prints the html content
 
+# Step 4: selecting wat we want to extract 
 
+# Extracting quotes, authors, and tags
+quotes = soup.find_all('div', class_='quote')
 
+# Looping through each quote and extracting the details
+for quote in quotes:
+    # Quote text
+    text = quote.find('span', class_='text').get_text()
+    
+    # Author name
+    author = quote.find('small', class_='author').get_text()
+    
+    # Tags associated with the quote
+    tags = [tag.get_text() for tag in quote.find_all('a', class_='tag')]
+    
+    # Print the details
+    print(f"Quote: {text}")
+    print(f"Author: {author}")
+    print(f"Tags: {', '.join(tags)}")
+    print("-" * 80)
